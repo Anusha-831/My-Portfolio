bb5dccadd64704e8670c56f8e5f3b7b1dda0c8b2
Anusha-831 <koyyadaanusha05@gmail.com>
2024-11-17 20:22:59 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*===========================================================typing animation======================================================*/
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Content Creator","YouTuber"],
+    strings:["","Web Designer","Web Developer","YouTuber"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
